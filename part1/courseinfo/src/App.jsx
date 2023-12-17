fb86acfc032846f2c3e803db7353039b0cd113f5
JimmyKurui <jimmy.chepkurui@strathmore.edu>
2023-12-17 13:13:41 +0300

    1.5 course info complete

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,31 +1,33 @@
-import Header from './Header'
-import Content from './Content'
-import Total from './Total'
+import Header from "./Header";
+import Content from "./Content";
+import Total from "./Total";
 
 const App = () => {
-  const course = "Half Stack Development"
-  const parts = [
-    {
-    name: "Fundamentals of React",
-    exercise : 10
-  },
-  {
-    name: "Using props to pass data",
-    exercise : 7
-  },
-  {
-    name: "State of a component",
-    exercise : 14
-  }
-]
-  
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercise: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercise: 7,
+      },
+      {
+        name: "State of a component",
+        exercise: 14,
+      },
+    ],
+  };
+
   return (
     <div>
-      <Header course={course}/>
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
