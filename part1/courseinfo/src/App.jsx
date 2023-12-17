eda0a0ba2dbfd845053951ead8ef76aee1d118c4
JimmyKurui <jimmy.chepkurui@strathmore.edu>
2023-12-17 12:51:47 +0300

    1.3 Complete

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -4,24 +4,26 @@ import Total from './Total'
 
 const App = () => {
   const course = "Half Stack Development"
-  const part1 = {
+  const parts = [
+    {
     name: "Fundamentals of React",
     exercise : 10
-  }
-  const part2 = {
+  },
+  {
     name: "Using props to pass data",
     exercise : 7
-  }
-  const part3 = {
+  },
+  {
     name: "State of a component",
     exercise : 14
   }
+]
   
   return (
     <div>
       <Header course={course}/>
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Total part1={part1} part2={part2} part3={part3} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
