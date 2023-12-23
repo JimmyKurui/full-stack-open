d2e0e4bc4f8f96fc8266c5993e337fce75ba909d
JimmyKurui <jimmy.chepkurui@strathmore.edu>
2023-12-23 12:14:48 +0300

    Unicafe exercises done
    Derived elements should be stateless

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,65 @@
+import { useState} from 'react'
+
+const Statistics = ({good, neutral, bad}) => {
+  const all = good + neutral + bad
+  const average = (good - bad)/ all
+  const positive = (good/all) * 100
+
+  if(all === 0) {
+    return (
+      <p>No feedback given</p>
+    )
+  }
+  return (
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={all} />
+        <StatisticLine text="average" value={average} />
+        <StatisticLine text="positive" value={positive + '%'} />
+      </tbody>
+    </table>
+  )
+}
+const StatisticLine = ({text, value}) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+
+const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGood = () => {
+    setGood(good + 1)
+  }
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+  const handleBad = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={handleGood} text="good" />
+      <Button handleClick={handleNeutral} text="neutral" />
+      <Button handleClick={handleBad} text="bad" />
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
+    </div>
+  )
+}
+
+export default App
